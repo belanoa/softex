6637d856fa3b9cc774a37c27ce0acf62ad5800a8
Andrea Belano <andrea.belano@gmail.com>
2024-05-12 17:53:44 +0200

    Fixed some errors during synthesis

--- rtl/expu/expu_correction.sv
+++ rtl/expu/expu_correction.sv
@@ -9,15 +9,15 @@ import softex_pkg::*;
 import fpnew_pkg::*;
 
 module expu_correction #(
-    parameter fpnew_pkg::fp_format_e    FPFORMAT                = FPFORMAT_IN               ,
-    parameter int unsigned              COEFFICIENT_FRACTION    = EXPU_COEFFICIENT_FRACTION ,
-    parameter int unsigned              CONSTANT_FRACTION       = EXPU_CONSTANT_FRACTION    ,
-    parameter int unsigned              MUL_SURPLUS_BITS        = EXPU_MUL_SURPLUS_BITS     ,
-    parameter int unsigned              NOT_SURPLUS_BITS        = EXPU_NOT_SURPLUS_BITS     ,
-    parameter real                      ALPHA_REAL              = EXPU_ALPHA_REAL           ,
-    parameter real                      BETA_REAL               = EXPU_BETA_REAL            ,
-    parameter real                      GAMMA_1_REAL            = EXPU_GAMMA_1_REAL         ,
-    parameter real                      GAMMA_2_REAL            = EXPU_GAMMA_2_REAL         ,
+    parameter fpnew_pkg::fp_format_e    FPFORMAT                = FPFORMAT_IN                   ,
+    parameter int unsigned              COEFFICIENT_FRACTION    = EXPU_COEFFICIENT_FRACTION     ,
+    parameter int unsigned              CONSTANT_FRACTION       = EXPU_CONSTANT_FRACTION        ,
+    parameter int unsigned              MUL_SURPLUS_BITS        = EXPU_MUL_SURPLUS_BITS         ,
+    parameter int unsigned              NOT_SURPLUS_BITS        = EXPU_NOT_SURPLUS_BITS         ,
+    parameter int unsigned              ALPHA_FIXED             = EXPU_ALPHA_FIXED              ,
+    parameter int unsigned              BETA_FIXED              = EXPU_BETA_FIXED               ,
+    parameter int unsigned              GAMMA_1_FIXED           = EXPU_GAMMA_1_FIXED            ,
+    parameter int unsigned              GAMMA_2_FIXED           = EXPU_GAMMA_2_FIXED            ,
 
     localparam int unsigned WIDTH   = fpnew_pkg::fp_width(FPFORMAT)
 ) (
@@ -32,14 +32,6 @@ module expu_correction #(
     logic [EXPONENT_BITS - 1 : 0]               exponent;
     //Q<1.MANTISSA_BITS>
     logic [MANTISSA_BITS : 0]                   mantissa;
-
-    //Q<-1.CONSTANT_FRACTION>
-    localparam int unsigned ALPHA   = int'(ALPHA_REAL * 2 ** COEFFICIENT_FRACTION);
-    localparam int unsigned BETA    = int'(BETA_REAL * 2 ** COEFFICIENT_FRACTION);
-
-    //Q<2.COEFFICIENT_FRACTION>
-    localparam int unsigned GAMMA_1 = int'(GAMMA_1_REAL * 2 ** CONSTANT_FRACTION);
-    localparam int unsigned GAMMA_2 = int'(GAMMA_2_REAL * 2 ** CONSTANT_FRACTION);
 
     //Q<-1.MANTISSA_BITS + MUL_SURPLUS_BITS>
     logic [MANTISSA_BITS - 2 + MUL_SURPLUS_BITS : 0]   mant_mul_1;
@@ -71,12 +63,12 @@ module expu_correction #(
     assign mantissa =   {1'b1, op_i [MANTISSA_BITS - 1 : 0]};
 
     assign mant_mul_1           = mantissa [MANTISSA_BITS - 1] == 1'b0 ? {mantissa [MANTISSA_BITS - 2 : 0], {MUL_SURPLUS_BITS{1'b0}}} : ~{mantissa [MANTISSA_BITS - 1 : 0], {MUL_SURPLUS_BITS{1'b0}}};
-    assign alpha_beta_mul_1     = mantissa [MANTISSA_BITS - 1] == 1'b0 ? ALPHA : BETA;
+    assign alpha_beta_mul_1     = mantissa [MANTISSA_BITS - 1] == 1'b0 ? ALPHA_FIXED : BETA_FIXED;
 
     assign res_mul_1            = mant_mul_1 * alpha_beta_mul_1;
 
     assign mant_add_1           = {mantissa, {(SUM_FRACTION - MANTISSA_BITS){1'b0}}};
-    assign gamma_add_1          = {mantissa [MANTISSA_BITS - 1] == 1'b0 ? GAMMA_1 : GAMMA_2, {(SUM_FRACTION - CONSTANT_FRACTION){1'b0}}};
+    assign gamma_add_1          = {mantissa [MANTISSA_BITS - 1] == 1'b0 ? GAMMA_1_FIXED : GAMMA_2_FIXED, {(SUM_FRACTION - CONSTANT_FRACTION){1'b0}}};
 
     assign res_add_1            = mant_add_1 + gamma_add_1;
 
