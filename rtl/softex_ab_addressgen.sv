8613a3d5c0e5ad77aef74b5c91e9d773bdbd6ecb
Andrea Belano <andrea.belano@studio.unibo.it>
2024-10-15 16:42:11 +0200

    Add weight streams to streamer

---
+++ rtl/softex_ab_addressgen.sv
@@ -0,0 +1,103 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+//
+// Andrea Belano <andrea.belano@studio.unibo.it>
+//
+
+import hwpe_stream_package::*;
+import hci_package::*;
+import softex_pkg::*;
+
+module softex_ab_addressgen #(
+    parameter int unsigned              N_ELEMS         = (DATA_W - 32) / WIDTH_IN  ,
+    parameter int unsigned              N_BLOCKS        = BUF_AB_BLOCKS             ,
+    parameter int unsigned              ELEM_WIDTH      = WIDTH_IN                  ,
+    parameter int unsigned              STRB_WIDTH      = 0                         ,
+    parameter int unsigned              CNT_WIDTH       = BUF_CNT_WIDTH             ,
+    parameter int unsigned              ELEM_READS      = NUM_REGS_FMA_IN                      
+) (
+    input   logic                   clk_i           ,
+    input   logic                   rst_ni          ,
+    input   logic                   clear_i         ,
+    input   ab_addressgen_ctrl_t    ctrl_i          ,
+    input   hci_streamer_flags_t    stream_flags_i  ,
+    output  hci_streamer_ctrl_t     stream_ctrl_o   
+);
+
+    enum logic [1:0]    { IDLE, FIRST_FILL, BACKWARD, FORWARD } current_state, next_state;
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin : state_register
+        if (~rst_ni) begin
+            current_state <= IDLE;
+        end else begin
+            if (clear_i) begin
+                current_state <= IDLE;
+            end else begin
+                current_state <= next_state;
+            end
+        end
+    end
+
+    always_comb begin : fsm
+        next_state      = current_state;
+        stream_ctrl_o   = '0;
+
+        stream_ctrl_o.addressgen_ctrl.d0_stride = N_ELEMS * ELEM_WIDTH / 8;
+        stream_ctrl_o.addressgen_ctrl.base_addr = ctrl_i.base_addr;
+
+        case (current_state)
+            IDLE: begin
+                if (ctrl_i.addressgen_start) begin
+                    next_state  = FIRST_FILL;
+
+                    stream_ctrl_o.addressgen_ctrl.tot_len   = ctrl_i.ab_buf_ctrl.num_blocks + 1;
+                    
+                    stream_ctrl_o.req_start = '1;
+                end
+            end
+
+            FIRST_FILL: begin
+                stream_ctrl_o.addressgen_ctrl.tot_len   = ctrl_i.ab_buf_ctrl.num_blocks + 1;
+
+                if (stream_flags_i.done) begin
+                    if (ctrl_i.ab_buf_ctrl.num_blocks == N_BLOCKS - 1) begin
+                        next_state  = IDLE;
+                    end else begin
+                        next_state  = BACKWARD;
+                    end
+                end
+            end
+
+            BACKWARD: begin
+                stream_ctrl_o.req_start                 = stream_flags_i.ready_start;
+                stream_ctrl_o.addressgen_ctrl.tot_len   = ctrl_i.ab_buf_ctrl.num_blocks + 1 - N_BLOCKS;
+                stream_ctrl_o.addressgen_ctrl.base_addr = ctrl_i.base_addr + (ctrl_i.ab_buf_ctrl.num_blocks - N_BLOCKS) * N_ELEMS * ELEM_WIDTH / 8;
+                stream_ctrl_o.addressgen_ctrl.d0_stride = -(N_ELEMS * ELEM_WIDTH / 8);
+
+                if (stream_flags_i.done) begin
+                    if (ctrl_i.x_done) begin
+                        next_state  = IDLE;
+                    end else begin
+                        next_state  = FORWARD;
+                    end
+                end
+            end
+
+            FORWARD: begin
+                stream_ctrl_o.req_start                 = stream_flags_i.ready_start;
+                stream_ctrl_o.addressgen_ctrl.tot_len   = ctrl_i.ab_buf_ctrl.num_blocks + 1 - N_BLOCKS;
+                stream_ctrl_o.addressgen_ctrl.base_addr = ctrl_i.base_addr + (N_ELEMS * ELEM_WIDTH / 8) * N_BLOCKS;
+
+                if (stream_flags_i.done) begin
+                    if (ctrl_i.x_done) begin
+                        next_state  = IDLE;
+                    end else begin
+                        next_state  = BACKWARD;
+                    end
+                end
+            end
+        endcase
+    end
+
+endmodule
--- rtl/softex_streamer.sv
+++ rtl/softex_streamer.sv
@@ -27,15 +27,21 @@ import softex_pkg::*;
     input   hci_streamer_ctrl_t     out_stream_ctrl_i   ,
     input   hci_streamer_ctrl_t     slot_in_ctrl_i      ,
     input   hci_streamer_ctrl_t     slot_out_ctrl_i     ,
+    input   hci_streamer_ctrl_t     a_in_stream_ctrl_i  ,
+    input   hci_streamer_ctrl_t     b_in_stream_ctrl_i  ,
     output  hci_streamer_flags_t    in_stream_flags_o   ,
     output  hci_streamer_flags_t    out_stream_flags_o  ,
     output  hci_streamer_flags_t    slot_in_flags_o     ,
     output  hci_streamer_flags_t    slot_out_flags_o    ,
+    output  hci_streamer_flags_t    a_in_stream_flags_o ,
+    output  hci_streamer_flags_t    b_in_stream_flags_o ,
 
     hwpe_stream_intf_stream.source  in_stream_o         ,
     hwpe_stream_intf_stream.sink    out_stream_i        ,
     hwpe_stream_intf_stream.source  slot_in_stream_o    ,
     hwpe_stream_intf_stream.sink    slot_out_stream_i   ,
+    hwpe_stream_intf_stream.source  a_in_stream_o       ,
+    hwpe_stream_intf_stream.source  b_in_stream_o       ,
 
     hci_core_intf.initiator         tcdm
 );
@@ -180,7 +186,7 @@ import softex_pkg::*;
 
     hci_core_intf #(
         .DW ( DW )
-    ) load_mux_i_tcdm [1:0] (
+    ) load_mux_i_tcdm [3:0] (
         .clk    (   clk_i   )
     );
 
@@ -214,6 +220,36 @@ import softex_pkg::*;
         .flags_o        (   slot_in_flags_o     )
     );
 
+    hci_core_source #(
+        .MISALIGNED_ACCESSES    (   1                            ),
+        .`HCI_SIZE_PARAM(tcdm)  (   `HCI_SIZE_PARAM(Tcdm_no_ecc) )
+    ) i_a_weight_in (
+        .clk_i          (   clk_i               ),
+        .rst_ni         (   rst_ni              ),
+        .test_mode_i    (   '0                  ),
+        .clear_i        (   clear_i             ),
+        .enable_i       (   enable_i            ),
+        .tcdm           (   load_mux_i_tcdm [2] ),
+        .stream         (   a_in_stream_o       ),
+        .ctrl_i         (   a_in_stream_ctrl_i  ),
+        .flags_o        (   a_in_stream_flags_o )
+    );
+
+    hci_core_source #(
+        .MISALIGNED_ACCESSES    (   1                            ),
+        .`HCI_SIZE_PARAM(tcdm)  (   `HCI_SIZE_PARAM(Tcdm_no_ecc) )
+    ) i_b_weight_in (
+        .clk_i          (   clk_i               ),
+        .rst_ni         (   rst_ni              ),
+        .test_mode_i    (   '0                  ),
+        .clear_i        (   clear_i             ),
+        .enable_i       (   enable_i            ),
+        .tcdm           (   load_mux_i_tcdm [3] ),
+        .stream         (   b_in_stream_o       ),
+        .ctrl_i         (   b_in_stream_ctrl_i  ),
+        .flags_o        (   b_in_stream_flags_o )
+    );
+
     hci_core_intf #(
         .DW ( DW )
     ) load_fifo (
@@ -221,7 +257,7 @@ import softex_pkg::*;
     );
 
     hci_core_mux_ooo #(
-        .NB_CHAN                (   2                            ),
+        .NB_CHAN                (   4                            ),
         .`HCI_SIZE_PARAM(out)   (   `HCI_SIZE_PARAM(Tcdm_no_ecc) )
     ) i_load_mux (
         .clk_i              (   clk_i           ),
@@ -342,8 +378,8 @@ import softex_pkg::*;
         .flags_o        (               ),
         .tcdm_target    (  store_fifo   ),
         .tcdm_initiator (  store_tcdm   )
-    ); 
-    
+    );
+
     hci_core_r_id_filter #(
         .`HCI_SIZE_PARAM(tcdm_target)   (   `HCI_SIZE_PARAM(Tcdm_no_ecc) )
     ) i_store_r_id_filter (
